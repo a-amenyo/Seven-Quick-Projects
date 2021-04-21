ab8ad8767c1ac016b1c543cf91661f3cfeeef2b6
yoooel <72613221+yoooel@users.noreply.github.com>
2021-04-21 16:53:20 +0000

    styled

--- Top-Singers/js/script.js
+++ Top-Singers/js/script.js
@@ -1,13 +1,13 @@
 let app = angular.module("SingersApp", []);
 app.controller("SingerController",function($scope){
     $scope.singers = [
-        {"pic": "images/1.jpg", "name": "drake", "gender": "male", "quality": "images/like.jpg"},
-        {"pic": "images/2.png", "name": "post-malon", "gender": "male", "quality": "images/dislike.jpg"},
-        {"pic": "images/3.jfif", "name": "taylor swift", "gender": "female", "quality": "images/like.jpg"},
-        {"pic": "images/4.jfif", "name": "micky-minaj", "gender": "female", "quality": "images/like.jpg"},
-        {"pic": "images/5.jfif", "name": "katty Perry", "gender": "female", "quality": "images/dislike.jpg"},
-        {"pic": "images/6.jfif", "name": "beyounce", "gender": "female", "quality": "images/dislike.jpg"},
+        {"pic": "images/jcole.jpg", "name": "J.Cole", "gender": "male", "quality": "images/like.jpg"},
+        {"pic": "images/kwesi.jpg", "name": "Kwesi Arthur", "gender": "male", "quality": "images/dislike.jpg"},
+        {"pic": "images/dreezy.png", "name": "Dreezy ", "gender": "female", "quality": "images/like.jpg"},
+        {"pic": "images/4.jfif", "name": "Nicki", "gender": "female", "quality": "images/like.jpg"},
+        {"pic": "images/yawtog.jpg", "name": "Yaw Tog", "gender": "male", "quality": "images/dislike.jpg"},
+        {"pic": "images/flo.jpg", "name": "Flo Mili", "gender": "female", "quality": "images/dislike.jpg"},
         {"pic": "images/7.jpg", "name": "Rihanna", "gender": "female", "quality": "images/dislike.jpg"},
-        {"pic": "images/8.jpg", "name": "bruno-mars", "gender": "male", "quality": "images/like.jpg"},
+        {"pic": "images/mole.jpg", "name": "Kofi Mole", "gender": "male", "quality": "images/like.jpg"},
     ];
 });
